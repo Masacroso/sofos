54dc2b1507b670e991593e0de0505fb8585f0b81
Masacroso <Masacroso@users.noreply.github.com>
2016-01-17 16:55:15 +0100

    Create contenido_del_bookmarklet.js

---
+++ contenido_del_bookmarklet.js
@@ -0,0 +1,24 @@
+//Este código debe ir minificado y dentro de una función anónima dentro de un bookmark:
+// URL del bookmark: javascript:(function(){AQUÍ VA EL CÓDIGO MINIFICADO DE ESTE ARCHIVO})();
+
+if (!($ = window.jQuery)) { // typeof jQuery=='undefined' works too
+    script = document.createElement( 'script' );
+   script.src = 'http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js'; 
+    script.onload=releasetheKraken;
+    document.body.appendChild(script);
+} 
+else {
+    releasetheKraken();
+}
+ 
+function releasetheKraken() {
+     var bb = document.createElement('link');
+   bb.type = 'text/css';
+  	bb.href = 'https://alojamiento/bunga.min.css';
+  bb.rel = 'stylesheet'
+  document.body.appendChild(bb);
+ 
+ var aa = document.createElement('script');
+  aa.src = 'https://alojamiento/sofos_tooltip.min.js';
+  document.body.appendChild(aa);
+}
